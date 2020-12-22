0ea3f2924063f56ac6e50b29393d47e3499aa39c
Koen Groot <kwgroot@gmail.com>
2020-12-22 14:00:27 +0100

    Setup done for creating and storing notes in a database.

--- app/src/main/java/com/example/madlevel5task1/NoteDao.kt
+++ app/src/main/java/com/example/madlevel5task1/NoteDao.kt
@@ -1,4 +1,19 @@
 package com.example.madlevel5task1
 
-class NoteDao {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface NoteDao {
+    @Query("SELECT * FROM noteTable LIMIT 1")
+    fun getNotepad(): LiveData<Note?>
+
+    @Insert
+    suspend fun insertNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
 }
